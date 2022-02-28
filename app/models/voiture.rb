a66c43804a782a4d3fed831e3c836a679e2b7a49
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-02-28 18:48:49 +0100

    Partie sur l'XML en cours

--- app/models/voiture.rb
+++ app/models/voiture.rb
@@ -1,4 +1,28 @@
 class Voiture < ApplicationRecord
     belongs_to :marque # Nom de la classe étrangère, en snake_case
     belongs_to :personne 
+
+    def self.data
+        contenu = File.read('./db/voitures.xml')
+        h = Hash.from_xml(contenu)
+        
+        h['voitures'].each do |data| 
+            t = Marque.where(libelle:data["marque"]["nom"])
+            id = 0
+            if (t.length > 0)
+                id = t.id 
+            else
+                m =  Marque.create(libelle:data["marque"]["nom"])
+                id = m.id
+            end
+
+            Voiture.create(
+                immatriculation: data['immatriculation'], 
+                couleur: data['couleur'],
+                marque_id: id,
+                modele: data['modele'],
+                date_mise_en_service: data['created_at'] 
+            )
+        end
+    end
 end
